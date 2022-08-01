91ce68566d25429c2b007847dd4cedc16668f52c
Ariana Ghandi <jacquesvdw2002@gmail.com>
2022-08-01 10:32:19 +0200

    Added files
    awdawd

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continueLoop = true;
+
+            Stopwatch sw = new Stopwatch();
+            Thread StopwatchSecondsThread = new Thread(sw.StopwatchSeconds);
+            Thread StopwatchMinutesThread = new Thread(sw.StopwatchMinutes);
+            StopwatchSecondsThread.Start();
+            StopwatchMinutesThread.Start();
+
+            while (continueLoop)
+            {
+                if (int.Parse(Console.ReadLine()) == 1)
+                {
+                    continueLoop = false;
+                    StopwatchSecondsThread.Abort();
+                    StopwatchMinutesThread.Abort();
+                }
+            }
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Stopwatch
+    {
+        int x = 0;
+        int y = 0;
+        bool continueLoop = true;
+
+        public Stopwatch()
+        {
+        }
+
+        public void StopwatchSeconds()
+        {
+            while (continueLoop)
+            {
+                Thread.Sleep(1000);
+                if (x >= 59)
+                {
+                    x = 1;
+                }
+                else
+                {
+                    x++;
+                }
+                Console.Clear();
+                Console.WriteLine(y + ":" + x);
+            }
+        }
+
+        public void StopwatchMinutes()
+        {
+            while (continueLoop)
+            {
+                Thread.Sleep(1000);
+                if (x >= 59)
+                {
+                    y++;
+                }
+            }
+        }
+    }
+}
