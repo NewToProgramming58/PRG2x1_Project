a06384183af8ec9609a1cbc19433b16dcf7eda4e
NewToProgramming58 <NewToProgramming58@users.noreply.github.com>
2022-08-03 11:21:34 +0200

    a

--- Project Milestone 2/SalesManger.cs
+++ Project Milestone 2/SalesManger.cs
@@ -59,5 +59,48 @@ namespace Project_Milestone_2
 
             return ds.Tables[0];
         }
+
+        //public DataTable FilterItems(List<string> filters)
+       // {
+            //List<string> fields = new List<string>();
+            //List<string> signs = new List<string>();
+            //List<string> values = new List<string>();
+
+            //foreach (string filter in filters)
+            //{
+            //    // 0 = field
+            //    // 1 = sign
+            //    // 2 = value
+            //    var splitFilters = filter.Split('#');
+            //    fields.Add(splitFilters[0]);
+            //    if (splitFilters[1].Equals("="))
+            //        signs.Add("LIKE");
+            //    else if (splitFilters[1].Equals("!="))
+            //        signs.Add("NOT LIKE");
+            //    else
+            //        signs.Add(splitFilters[1]);
+
+            //    if (splitFilters[0].Equals("Price") || splitFilters[0].Equals("Category"))
+            //        values.Add(splitFilters[2]);
+            //    else if ((splitFilters[1].Equals("LIKE") || splitFilters[1].Equals("NOT LIKE")) && !splitFilters[1].Equals("="))
+            //        values.Add($"'%{splitFilters[2]}%'");
+            //    else
+            //        values.Add($"'{splitFilters[2]}'");
+            //}
+            //string cmdString = $"SELECT I.ItemID, I.ItemName, I.Price, I.Quantity, C.Category FROM Items AS I INNER JOIN Category AS C ON I.CategoryID = C.CategoryID WHERE I.{fields[0]} {signs[0]} {values[0]}";
+            //if (fields.Count > 1)
+            //{
+            //    for (int i = 1; i < signs.Count; i++)
+            //    {
+            //        cmdString += $" AND I.{fields[i]} {signs[i]} {values[i]}";
+            //    }
+            //}
+            //SqlDataAdapter dataAdapter = new SqlDataAdapter(cmdString, sqlConnection);
+
+            //DataSet ds = new DataSet();
+            //dataAdapter.Fill(ds);
+
+            //return ds.Tables[0];
+
+        //}
     }
-}
